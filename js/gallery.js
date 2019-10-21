8009bc01774d21e195f2babb239afdfdc02c84df
Sergey <ryc-sergei@yandex.ru>
2019-10-21 22:49:58 +0300

    Реализация фильтров

--- js/gallery.js
+++ js/gallery.js
@@ -9,21 +9,8 @@
 
   var onSuccess = function (pictures) {
     // console.log('Картинки получнены');
-    var pictureContainer = document.querySelector('.pictures');
-    var picturesFragment = document.createDocumentFragment();
-
-    for (var i = 0; i < pictures.length; i++) {
-      picturesFragment.appendChild(window.getPictureElement(pictures[i], i));
-    }
-    pictureContainer.appendChild(picturesFragment);
-
-    pictureContainer.addEventListener('click', function (evt) {
-      var target = evt.target;
-      if (target.classList.contains('picture__img') || target.classList.contains('picture')) {
-        var photoID = target.getAttribute('data-id');
-        window.showBigPicture(pictures[photoID]);
-      }
-    });
+    window.renderPictures(pictures);
+    window.showFilters(pictures);
   };
   // console.log('Делаем запрос на сервер');
   window.load(onSuccess, onError);
